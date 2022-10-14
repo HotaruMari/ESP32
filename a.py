ad82d916c7d4fd6798d5fea7d60bf63023fe33ee
HotaruMari <marisela12santi@gmail.com>
2022-10-14 16:22:49 -0500

    Blink

--- a.py
+++ a.py
@@ -2,16 +2,6 @@ from machine import Pin
 from time import sleep_ms as slms
 import network
 
-def do_connect():
-    
-    wlan = network.WLAN(network.STA_IF) # create station interface
-    wlan.active(True)   # activate the interface
-    if not wlan.isconnected(): #if station interface conect with a new one
-        print('connecting to network...')
-        wlan.connect('Marisenal', 'Mariesgenial')  # connect to an AP
-        while not wlan.isconnected(): #waiting conection
-            pass
-    print('network config:', wlan.ifconfig()) # get the interface's IP/netmask/gw/DNS addresses
 def toggle(max):
     lap=0
     while lap<max:
@@ -24,9 +14,6 @@ def toggle(max):
 led = Pin(2,Pin.OUT)
 
 #MAIN-------------------------------------------------------
-print('Starting connection')
-do_connect()
-print('Connected')
 print('Blink')
 toggle(5)
 print('OVER')
